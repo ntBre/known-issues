9a15ebd8e8811ce721479ddab116802ed24f73d5
Brent Westbrook <brentrwestbrook@gmail.com>
2023-08-16 10:16:13 -0400

    switch to beamer and play with caption order and figure size

--- latex.py
+++ latex.py
@@ -4,13 +4,13 @@ from io import StringIO
 
 class Latex:
     def __init__(self):
-        self._num_images = 0
         self._buf = StringIO()
         self._buf.write(
-            r"""\documentclass{article}
+            r"""\documentclass{beamer}
 
 \usepackage{graphicx}
-\usepackage[margin=1in]{geometry}
+
+\setbeamertemplate{navigation symbols}{}
 
 \begin{document}
 """
@@ -20,25 +20,22 @@ class Latex:
         self._buf.write(r"\end{document}")
 
     def add_image(self, filename: str, caption: str):
-        self._num_images += 1
-        self._buf.write(
-            rf"""
-\begin{{figure}}[ht]
-\centering
-    \includegraphics{{{filename}}}
-\end{{figure}}
-"""
-        )
+        self._buf.write(r"\begin{frame}[fragile]" + "\n")
+
         MAX_LEN = 40
         end = int(math.ceil(len(caption) / MAX_LEN))
         for i in range(end):
-            start = MAX_LEN * i
-            end = MAX_LEN * (i + 1)
-            cap = caption[start:end]
+            cap = caption[MAX_LEN * i : MAX_LEN * (i + 1)]
             self._buf.write(rf"\verb|{cap}|" + "\n")
 
-        if self._num_images % 20 == 0:
-            self._buf.write(r"\clearpage" + "\n")
+        self._buf.write(
+            rf"""
+\begin{{figure}}
+    \includegraphics[width=0.7\textwidth,height=0.7\textheight,keepaspectratio]{{{filename}}}
+\end{{figure}}
+"""
+        )
+        self._buf.write(r"\end{frame}" + "\n")
 
     def to_file(self, filename):
         self._finalize()
