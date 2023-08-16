b6ef5d61eba34c3cb41e05b9d69ae0f06ec233fe
Brent Westbrook <brentrwestbrook@gmail.com>
2023-08-16 07:54:42 -0400

    move SMILES out of captions and inject clearpage every 20 images

--- latex.py
+++ latex.py
@@ -4,12 +4,12 @@ from io import StringIO
 
 class Latex:
     def __init__(self):
+        self._num_images = 0
         self._buf = StringIO()
         self._buf.write(
             r"""\documentclass{article}
 
 \usepackage{graphicx}
-\usepackage{cprotect}
 \usepackage[margin=1in]{geometry}
 
 \begin{document}
@@ -20,12 +20,14 @@ class Latex:
         self._buf.write(r"\end{document}")
 
     def add_image(self, filename: str, caption: str):
+        self._num_images += 1
         self._buf.write(
             rf"""
 \begin{{figure}}[ht]
 \centering
     \includegraphics{{{filename}}}
-\cprotect\caption{{"""
+\end{{figure}}
+"""
         )
         MAX_LEN = 40
         end = int(math.ceil(len(caption) / MAX_LEN))
@@ -33,14 +35,10 @@ class Latex:
             start = MAX_LEN * i
             end = MAX_LEN * (i + 1)
             cap = caption[start:end]
-            self._buf.write(rf"\verb|{cap}| ")
+            self._buf.write(rf"\verb|{cap}|\n")
 
-        # close the caption, end the figure
-        self._buf.write(
-            r"""}
-\end{figure}
-"""
-        )
+        if self._num_images % 20 != 0:
+            self._buf.write(r"\clearpage")
 
     def to_file(self, filename):
         self._finalize()
